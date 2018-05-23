95989494430e451baa7a985812f2c281192d478c
Thong <thongnx6@gmail.com>
2018-05-23 15:34:16 +0700

    Update app routes and integrate hashtag screen
    - Update app use style scss
    - Config module use app-core, app-shared, mn-core and blocks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+/// <reference path="../typings.d.ts" />
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { MnCoreModule } from '@mn-core/mn-core.module';
+import { AppCoreModule } from '@app-core/app-core.module';
+import { AppSharedModule } from '@app-shared/app-shared.module';
 
 import { AppComponent } from './app.component';
-
+import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MnCoreModule.forRoot(),
+    AppSharedModule.forRoot(),
+    AppCoreModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
